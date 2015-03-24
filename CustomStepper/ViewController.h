a9753334322c72b14268c7477d8a3669bd22b7c4
lihualin <lihualin@lihualindeMac-mini.local>
2015-03-24 11:51:19 +0800

    Initial Commit

---
+++ CustomStepper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomStepper
+//
+//  Created by lihualin on 15/3/24.
+//  Copyright (c) 2015年 lihualin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomStepper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomStepper
+//
+//  Created by lihualin on 15/3/24.
+//  Copyright (c) 2015年 lihualin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
